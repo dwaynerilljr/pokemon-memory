b01b1e41cf3d47b5fceda3b1cc2697494f4a7efa
Dwayne Rill Jr <dwayne@dwaynerilljr.dev>
2022-12-19 21:13:08 -0500

    Design improvements Made some design improvements; Added in a turn counter; Smoothed out animations; Finished project;

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
   const [choiceOne, setChoiceOne] = useState(null);
-  const [choiceTwo, setChoiceTwo] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
 
   //shuffle cards
   const shuffleCards = () => {
@@ -23,6 +24,8 @@ function App() {
       .sort(() => Math.random() - 0.5)
       .map((card) => ({...card, id: Math.random()}))
 
+      setChoiceOne(null);
+      setChoiceTwo(null);
       setCards(shuffledCards)
       setTurns(0)
   }
@@ -35,7 +38,7 @@ function App() {
   //compare 2 selected cards
   useEffect(() => {
     if (choiceOne && choiceTwo) {
-      
+      setDisabled(true)
       if (choiceOne.src === choiceTwo.src) {
         setCards(prevCards => {
           return prevCards.map(card => {
@@ -54,18 +57,24 @@ function App() {
     }
   }, [choiceOne, choiceTwo])
 
-  console.log(cards)
-
+  //reset turns
   const resetTurn = () => {
     setChoiceOne(null);
     setChoiceTwo(null);
     setTurns(prevTurns => prevTurns + 1);
+    setDisabled(false)
   }
+
+  //start a new game automatically
+  useEffect(() => {
+    shuffleCards();
+  }, [])
 
   return (
     <div className="App">
       <h1 id='title'>Pokémon Memory!</h1>
       <button onClick={shuffleCards}>New Game</button>
+      <p>Turns: {turns}</p>
       <div className="card-grid">
         {cards.map(card => (
           <SingleCard 
@@ -73,6 +82,7 @@ function App() {
             card={card}
             handleChoice={handleChoice}
             flipped={card === choiceOne || card === choiceTwo || card.matched === true}
+            disabled={disabled}
           />
         ))}
       </div>
